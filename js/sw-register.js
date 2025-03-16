9085cf17fffee311f4373d5f8e73bcc6823ec556
Pushkar Srivastav <privategafbusiness@gmail.com>
2025-03-16 21:30:00 +0530

    Update sw-register.js

--- js/sw-register.js
+++ js/sw-register.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/dona/Calculator/sw-register.js')
+    navigator.serviceWorker.register('/tester/js/sw-register.js')
     .then(() => console.log("Service Worker Registered!"))
     .catch(err => console.error("Service Worker Registration Failed:", err));
 }
