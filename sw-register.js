69ab1230fe59d06ab75a0a25fc56442ccea5c738
Joya <privategafbusiness@gmail.com>
2025-03-17 17:39:41 +0530

    Delete sw-register.js

--- sw-register.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/dona/Calculator/sw-register.js')
-    .then(() => console.log("Service Worker Registered!"))
-    .catch(err => console.error("Service Worker Registration Failed:", err));
-}
